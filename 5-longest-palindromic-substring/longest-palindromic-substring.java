935a82451ba3db36ceb90fa559e9ee156cc7e989
shivam-kumar080 <shivam.23scse1180221@galgotiasuniversity.ac.in>
2026-05-12 10:46:52 +0530

    Time: 2590 ms (5.00%) | Memory: 47.1 MB (14.14%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPallindrome(String sub) {
+        int i = 0;
+        int j = sub.length() - 1;
+        while (i < j) {
+            if (sub.charAt(i) != sub.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int maxLen = 0;
+        String result = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                String sub = s.substring(i, j + 1);
+                if (isPallindrome(sub) == true) {
+                    if (sub.length() > maxLen) {
+                        maxLen = sub.length();
+                        result = sub;
+                    }
+                }
+
+            }
+        }
+        return result;
+
+    }
+}
